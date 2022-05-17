d7bc8c348ebdf0a8911e63fc420a024b58357821
Orvyl Tumaneng <orvyl.t@gmail.com>
2022-05-17 16:21:19 +0800

    created Main commandLIneRunner

--- day2/book-service-v2/src/main/java/com/example/bookservicev2/service/TimestampIdGenerator.java
+++ day2/book-service-v2/src/main/java/com/example/bookservicev2/service/TimestampIdGenerator.java
@@ -3,11 +3,14 @@ package com.example.bookservicev2.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @Service
 @Profile("timestamp")
 public class TimestampIdGenerator implements IdGenerator {
     @Override
     public String getNext() {
-        return null;
+        return ""+LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli();
     }
 }
--- day2/book-service-v2/src/main/java/com/example/bookservicev2/service/UUIDFormatIdGenerator.java
+++ day2/book-service-v2/src/main/java/com/example/bookservicev2/service/UUIDFormatIdGenerator.java
@@ -3,11 +3,13 @@ package com.example.bookservicev2.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @Profile("uuid")
 public class UUIDFormatIdGenerator implements IdGenerator{
     @Override
     public String getNext() {
-        return null;
+        return UUID.randomUUID().toString();
     }
 }
